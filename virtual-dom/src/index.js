4ed840a430a2cd3bc2f2a450fc1ce3039c465913
saeedhassansolangi <saeedhassansolangi@gmail.com>
2020-05-19 17:00:29 +0500

    added images

--- virtual-dom/src/index.js
+++ virtual-dom/src/index.js
@@ -5,6 +5,7 @@ const tick=()=>{
  ReactDOM.render([
     React.createElement("div",null,"Hello World"),
     React.createElement("input",null),
-    React.createElement("p",null,(new Date().toLocaleTimeString()))],document.getElementById("root"))
+    React.createElement("p",null,(new Date().toLocaleTimeString())),
+    React.createElement("h1",null,"With Virtual DOM")],document.getElementById("root"))
 }
 setInterval(tick, 1000)
